7aae5390e0320f10d4b9dcc06520f199add44919
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2009-07-09 21:43:36 +0000

    essage

---
+++ phpLearningFromBeginning/ch20/templates_c/%%12^125^12549F31%%20-2.tpl.php
@@ -0,0 +1,16 @@
+<?php /* Smarty version 2.6.15, created on 2008-01-06 13:41:59
+         compiled from 20-2.tpl */ ?>
+ 
+<html>
+<head>
+<title><?php echo $this->_tpl_vars['page_title']; ?>
+</title>
+</head> 
+
+<body>
+大家好，我是<?php echo $this->_tpl_vars['name']; ?>
+模板引擎, 欢迎大家在PHP程序中使用<?php echo $this->_tpl_vars['name']; ?>
+。 
+</body>
+
+</html>
