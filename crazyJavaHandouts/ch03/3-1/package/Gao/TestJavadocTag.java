910effd1cdef887d07027952890b16fdbde45d68
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2009-07-27 11:15:03 +0000

    essage

---
+++ crazyJavaHandouts/ch03/3-1/package/Gao/TestJavadocTag.java
@@ -0,0 +1,23 @@
+package Gao;
+
+/**
+ * Description:
+ * <br/>Copyright (C), 2008-2009, FeiGao
+ * <br/>This program is protected by copyright laws.
+ * <br/>Program Name:TestJavadocTag.java
+ * <br/>Date:2009-07-27
+ * @author  FeiGao gaofeitop@gmail.com
+ * @version  1.0
+ */
+public class TestJavadocTag
+{
+    /**
+     * 一个得到打招呼字符串的方法。
+     * @param name 该参数指定向谁打招呼。
+     * @return 返回打招呼的字符串。
+     */
+    public String hello(String name)
+    {
+        return name + "，你好！";
+    }
+}
