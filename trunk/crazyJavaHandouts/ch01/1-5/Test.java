bae283de1076602abb87a175bcc4644388e81949
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2009-07-27 11:00:14 +0000

    essage

---
+++ trunk/crazyJavaHandouts/ch01/1-5/Test.java
@@ -0,0 +1,16 @@
+/**
+ * Description:
+ * <br/>Copyright (C), 2008-2009, FeiGao
+ * <br/>This program is protected by copyright laws.
+ * <br/>Program Name:Test.java
+ * <br/>Date:2009-07-27
+ * @author  FeiGao gaofeitop@gmail.com
+ * @version  1.0
+ */
+class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("这是Test类");
+    }
+}
--- trunk/crazyJavaHandouts/ch03/3-1/CommentTest.java
+++ trunk/crazyJavaHandouts/ch03/3-1/CommentTest.java
@@ -1,10 +1,22 @@
-public class CommentTest{
+/**
+ * Description:
+ * <br/>Copyright (C), 2008-2009, FeiGao
+ * <br/>This program is protected by copyright laws.
+ * <br/>Program Name:CommentTest.java
+ * <br/>Date:2009-07-27
+ * @author  FeiGao gaofeitop@gmail.com
+ * @version  1.0
+ */
+public class CommentTest
+{
     /**
-     * 这是多行注释
-     */
-    public static void main(String[] args){
-        //这是简单的注释
-        System.out.println("Hello world!");
+     * 这里面的内容全部是多行注释
+     * Java语言真的很有趣，
+    */
+    public static void main(String[] args)
+    {
+        //这是一行简单的注释
+        System.out.println("Hello World!");
         //System.out.println("此行被注释，将不会被编译执行");
     }
 }
