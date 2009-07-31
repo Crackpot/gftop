819e75923183ca6a8729c19e1f4164f023ab78d7
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2009-07-31 16:57:06 +0000

    essage

--- trunk/CrazyJavaHandoutsCodes/codes/convert.sh
+++ trunk/CrazyJavaHandoutsCodes/codes/convert.sh
@@ -1,6 +1,11 @@
-find ./ -type f -name "*.html"|while read line;do
+#假设需要将所有ipaddr目录下的java文件，编码从gb2312转到utf8
+#cd ipaddr
+find ./ -type f -name "*.java"|while read line;do
 echo $line
-iconv -f GBK -t UTF-8 $line > ${line}.utf8
+iconv -f GB2312 -t UTF-8 $line > ${line}.utf8
 mv $line ${line}.gb2312
 mv ${line}.utf8 $line
-done 
+done
+#上面脚本将当前ipaddr目录下的所有java文件，从gb2312转到utf8，原文保存为*.php.gb2312。
+#如果需要删除原来的gb2312文件，只需执行：
+find ./ -type f -name "*.gb2312" -exec rm -f {} \; 
