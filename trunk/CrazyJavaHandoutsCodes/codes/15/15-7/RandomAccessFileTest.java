3416c85e0fd8b9966c616eab19e332a09268b28d
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2009-07-31 15:29:32 +0000

    essage

--- trunk/CrazyJavaHandoutsCodes/codes/15/15-7/RandomAccessFileTest.java
+++ trunk/CrazyJavaHandoutsCodes/codes/15/15-7/RandomAccessFileTest.java
@@ -16,20 +16,20 @@ public class RandomAccessFileTest
 		RandomAccessFile raf = null;
 		try
 		{
-			//��ֻ����ʽ��һ��RandomAccessFile����
+			//以只读方式打开一个RandomAccessFile对象
 			raf = new RandomAccessFile("RandomAccessFileTest.java" , "r");
-			//��ȡRandomAccessFile�����ļ�ָ���λ�ã���ʼλ����0
-			System.out.println("RandomAccessFile���ļ�ָ��ĳ�ʼλ�ã�" 
+			//获取RandomAccessFile对象文件指针的位置，初始位置是0
+			System.out.println("RandomAccessFile的文件指针的初始位置：" 
 				+ raf.getFilePointer());
-			//�ƶ�raf���ļ���¼ָ���λ��
+			//移动raf的文件记录指针的位置
 			raf.seek(300);
 			byte[] bbuf = new byte[1024];
-			//���ڱ���ʵ�ʶ�ȡ���ֽ���
+			//用于保存实际读取的字节数
 			int hasRead = 0;
-			//ʹ��ѭ�����ظ���ȡˮ������
+			//使用循环来重复“取水”过程
 			while ((hasRead = raf.read(bbuf)) > 0 )
 			{
-				//ȡ������Ͳ����ˮ�Σ��ֽڣ������ֽ�����ת�����ַ��������
+				//取出“竹筒”中水滴（字节），将字节数组转换成字符串输出！
 				System.out.print(new String(bbuf , 0 , hasRead ));
 			}
 		}
