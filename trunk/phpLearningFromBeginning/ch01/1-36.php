6e83b28ec38a00d1815fe66ff1c0f4a660ec56d6
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2009-08-01 14:02:03 +0000

    essage

--- trunk/phpLearningFromBeginning/ch01/1-36.php
+++ trunk/phpLearningFromBeginning/ch01/1-36.php
@@ -1,4 +1,12 @@
-大家好，我是 <?php echo $_POST['user_name'];?>！
-<?php echo "<br/>" ?>
-<?php echo "<br/>" ?>
-我来自<?php echo $_POST['hometown'];?>。 
+<meta http-equiv="Content-Type" content="text/html; charset=utf8" />
+<?php
+    if($_POST["username"] && $_POST["hometown"])//两个信息都不为空
+    {
+        echo "大家好，我是", $_POST["name"] , "<br/>";   
+        //echo $_POST["username"] + "<br/>";   
+        echo "我来自",$_POST["hometown"], "<br>";
+    }
+    else{
+        echo "您未提交任何数据！请从提交表单重新提交信息";   
+    }
+?>
