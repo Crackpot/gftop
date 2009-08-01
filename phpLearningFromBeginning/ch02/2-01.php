97de4a968e6039144576a8de93fbb30ac330d0bc
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2009-08-01 14:52:57 +0000

    essage

---
+++ phpLearningFromBeginning/ch02/2-01.php
@@ -0,0 +1,8 @@
+<?php
+    echo "test string1";
+    echo "<br/>";                       //这里添加"<br/>"是为了在Web页面上生成新的一行，即换行
+    echo "<br/>";
+    echo "test string2<br/>";
+    //echo "test string3<br/>";
+    echo "test string4<br/>";
+?>
