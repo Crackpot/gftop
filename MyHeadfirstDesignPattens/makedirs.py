83be04d9c62558177827593aee6ea44a410a8e50
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2009-10-29 21:56:32 +0000

    essage

---
+++ MyHeadfirstDesignPattens/makedirs.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+import os
+startChapter = 1
+endChapter = 14
+for i in range(startChapter,endChapter):
+    if i < 10:
+        str_i = "0" + str(i)
+    else:
+        str_i = str(i)
+    path = './ch'+str_i
+    if not os.path.exists(path):
+        print "创建路径 ./ch"+str_i
+        os.mkdir(path)
