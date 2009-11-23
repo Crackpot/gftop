1365b60c3707181ee9580360ac0d19c792b75127
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2009-11-23 20:02:42 +0000

    essage

--- trunk/yjswjd/sql/createDatabase.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS yjswjd DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT ALL PRIVILEGES ON `yjswjd` . * TO 'crackpot'@'127.0.0.1';
-GRANT ALL PRIVILEGES ON `yjswjd` . * TO 'crackpot'@'localhost';
-GRANT ALL PRIVILEGES ON `yjswjd` . * TO 'crackpot'@'linux-Crackpot';
-USE yjswjd;
--- trunk/yjswjd/sql/yjswjd_consumption.sql
+++
@@ -1,6 +0,0 @@
-#会员消费表
-DROP TABLE yjswjd_consumption;
-CREATE TABLE IF NOT EXISTS yjswjd_consumption(
-    consumption_id INT NOT NULL AUTO_INCREMENT,
-
-);
