f9f53213a407ab57e1db99854a194cd0f3cf650e
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-01-28 15:02:50 +0000

    essage

--- trunk/Practical_Django_Projects/SimpleCms/search/admin.py
+++
@@ -1,7 +0,0 @@
-# coding=utf8
-from django.contrib import admin
-from SimpleCms.search.models import SearchKeyword
-
-class SearchKeywordAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(SearchKeyword, SearchKeywordAdmin)
