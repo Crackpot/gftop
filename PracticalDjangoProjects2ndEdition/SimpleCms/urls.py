ed569fd5597b5e31825110ed104bcb31bbd462de
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-02-03 04:43:52 +0000

    essage

---
+++ PracticalDjangoProjects2ndEdition/SimpleCms/urls.py
@@ -0,0 +1,23 @@
+# coding=utf8
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^SimpleCms/', include('SimpleCms.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+    (r'^search$','SimpleCms.search.views.search'), # 去掉斜杠
+    (r'^$','SimpleCms.views.index'),
+    (r'^tiny_mce/(?P<path>.*)$', 'django.views.static.serve',
+        { 'document_root': '/home/workspace/gftop/PracticalDjangoProjects2ndEdition/new/SimpleCms/javascripts/tinymce/jscripts/tiny_mce' }),
+    (r'', include('django.contrib.flatpages.urls')),
+)
