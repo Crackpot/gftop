8400777d3b1a6abc589021c889ea1c84768fc5da
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-02-04 08:50:55 +0000

    essage

--- PracticalDjangoProjects2ndEdition/SimpleCms/coltrane/views.py
+++ PracticalDjangoProjects2ndEdition/SimpleCms/coltrane/views.py
@@ -1,7 +1,7 @@
 # coding=utf8
 # Create your views here.
 from django.shortcuts import get_object_or_404, render_to_response
-from coltrane.models import Entry
+from coltrane.models import Entry, Category
 
 def entries_index(request):
     return render_to_response('coltrane/entry_index.html',
@@ -18,3 +18,12 @@ def entry_detail(request, year, month, day, slug):
                 slug = slug)
     return render_to_response('coltrane/entry_detail.html',
             { 'entry': entry })
+
+def category_list(request):
+    return render_to_response('coltrane/category_list.html',
+            { 'object_list': Category.objects.all() })
+def category_detail(request, slug):
+    category = get_object_or_404(Category, slug = slug)
+    return render_to_response('coltrane/category_detail.html', {
+                'object_list': category.entry_set.all(),
+                'category': category })
