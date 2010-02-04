3a4fb926f489846308fede6a4778ac57e4956908
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-02-04 05:30:53 +0000

    essage

--- trunk/PracticalDjangoProjects2ndEdition/SimpleCms/coltrane/views.py
+++ trunk/PracticalDjangoProjects2ndEdition/SimpleCms/coltrane/views.py
@@ -1,1 +1,8 @@
+# coding=utf8
 # Create your views here.
+from django.shortcuts import render_to_response
+from coltrane.models import Entry
+
+def entries_index(request):
+    return render_to_response('coltrane/entry_index.html',
+            { 'entry_list': Entry.objects.all() })
