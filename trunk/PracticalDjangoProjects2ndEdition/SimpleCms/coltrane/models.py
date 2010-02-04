6a5706f6af03e4f1c4b493e8a567863b34b1a8e2
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-02-04 08:50:55 +0000

    essage

--- trunk/PracticalDjangoProjects2ndEdition/SimpleCms/coltrane/models.py
+++ trunk/PracticalDjangoProjects2ndEdition/SimpleCms/coltrane/models.py
@@ -20,6 +20,7 @@ from django.db import models
 
 from markdown import markdown
 from tagging.fields import TagField
+from django.conf import settings
 
 class Category(models.Model):
     title = models.CharField(max_length = 250,
@@ -96,3 +97,52 @@ class Entry(models.Model):
             'slug': self.slug })
     get_absolute_url = models.permalink(get_absolute_url)
 
+class Link(models.Model):
+    # 元数据
+    enable_comments = models.BooleanField(default = True)
+    post_elsewhere = models.BooleanField('Post to del.icio.us',
+            default = True,
+            help_text = 'If checked, this link will be posted both to your weblog and to your del.icio.us account.')
+    posted_by = models.ForeignKey(User)
+    pub_date = models.DateTimeField(default = datetime.datetime.now)
+    slug = models.SlugField(unique_for_date = 'pub_date',
+            help_text = 'Must be unique for the publication date.')
+    title = models.CharField(max_length = 250)
+
+    # 真正链接数据
+    description = models.TextField(blank = True)
+    description_html = models.TextField(blank = True)
+    via_name = models.CharField('Via', max_length = 250, blank = True,
+            help_text = 'The name of the person whose site you spotted the link on. Optional.')
+    via_url = models.URLField('Via URL', blank = True,
+            help_text = 'The URL of the site where you spotted the link. Optional.')
+    tags = TagField()
+    url = models.URLField('URL', unique = True)
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __unicode__(self):
+        return self.title
+
+    def save(self):
+        if not self.id and self.post_elsewhere:
+            import pydelicious
+            from django.utils.encoding import smart_str
+            pydelicious.add(
+                    settings.DELICIOUS_USER,
+                    settings.DELICIOUS_PASSWORD,
+                    smart_str(self.url),
+                    smart_str(self.title),
+                    smart_str(self.tags))
+        if self.description:
+            self.description_html = markdown(self.description)
+        super(Link, self).save()
+
+    def get_absolute_url(self):
+        return ('coltrane_link_detail', (), {
+            'year': self.pub_date.strftime('%Y'),
+            'month': self.pub_date.strftime('%b').lower(),
+            'day': self.pub_date.strftime('%d'),
+            'slug': self.slug })
+    get_absolute_url = models.permalink(get_absolute_url)
