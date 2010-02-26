c890b1874b07cc3f4070fdad21c899dd68f28203
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-02-26 13:51:28 +0000

    essage

---
+++ trunk/c++BianChengJinDian/chapter01/fig01_02.cpp
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  fig01_02.cpp
+ *
+ *    Description:  打印一行文字
+ *
+ *        Version:  1.0
+ *        Created:  2010年02月12日 23时53分04秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  高飞 (gaofeitop@gmail.com), 
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+int main(){
+    std::cout << "Welcome to C++ !\n";
+    return 0; // indicate that program ended successfully
+}
