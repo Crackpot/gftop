7c49e31bc79d36370086ad7a616b3d9bda587d81
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-06-07 16:18:53 +0000

    essage

--- trunk/DjangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/search/views.py
+++ trunk/DjangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/search/views.py
@@ -2,16 +2,19 @@
 from django.http import HttpResponse
 from django.template import loader, Context
 from django.contrib.flatpages.models import FlatPage
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 
 def search(request):
     query = request.GET.get('q', '') # 取得提交的查询关键词
-    keyword_results = []
-    results = []
+    keyword_results = results = []
     if query:
         # 查询了东西
-        keyword_results = FlatPage.objects.filter(searchkeyword__keyword__in=query.split()).distinct()
-        results = FlatPage.objects.filter(content__icontains=query)
+        keyword_results = FlatPage.objects.filter(searchkeyword__keyword__in = query.split()).distinct()
+        if keyword_results.count() == 1:
+            # 只有一个就直接转向
+            return HttpResponseRedirect(keyword_results[0].get_absolute_url())
+        results = FlatPage.objects.filter(content__icontains = query)
     return render_to_response('search/search.html',
                               { 'query': query,
                                'keyword_results': keyword_results,
