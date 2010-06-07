6cd8a0abc19634c9cdc3cd4821ed094883713f9c
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-06-07 08:05:50 +0000

    essage

--- DjangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/Cms/search/models.py
+++ DjangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/Cms/search/models.py
@@ -1,3 +1,9 @@
+#coding=utf8
 from django.db import models
+from django.contrib.flatpages.models import FlatPage
 
-# Create your models here.
+class SearchKeyword(models.Model):
+    keyword = models.CharField('关键词', max_length = 50)
+    page = models.ForeignKey(FlatPage)
+    def __unicode__(self):
+        return self.keyword
