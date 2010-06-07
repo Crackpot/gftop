c234db215e4b35f2066b0e8361b82b29219e4278
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-06-07 10:05:13 +0000

    essage

--- DjangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/search/views.py
+++ DjangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/search/views.py
@@ -3,12 +3,16 @@ from django.http import HttpResponse
 from django.template import loader, Context
 from django.contrib.flatpages.models import FlatPage
 from django.shortcuts import render_to_response
+
 def search(request):
-    query = request.GET.get('q', '')
+    query = request.GET.get('q', '') # 取得提交的查询关键词
+    keyword_results = []
     results = []
     if query:
+        # 查询了东西
+        keyword_results = FlatPage.objects.filter(searchkeyword__keyword__in=query.split()).distinct()
         results = FlatPage.objects.filter(content__icontains=query)
     return render_to_response('search/search.html',
-    { 'query': query,
-    'results': results })
-
+                              { 'query': query,
+                               'keyword_results': keyword_results,
+                               'results': results })
