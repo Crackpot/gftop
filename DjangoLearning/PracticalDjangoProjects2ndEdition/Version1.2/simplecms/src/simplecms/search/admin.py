cec9a35ad8fb4623217d25f1b55ee03fe0b05f04
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-06-07 09:46:57 +0000

    essage

--- DjangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/search/admin.py
+++ DjangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/search/admin.py
@@ -1,3 +1,17 @@
 #coding=utf8
 from django.contrib import admin
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+
 from simplecms.search.models import SearchKeyword
+
+class SearchKeywordInline(admin.StackedInline):
+    model = SearchKeyword
+    
+class FlatPageAdminWithKeywords(FlatPageAdmin):
+    inlines = [SearchKeywordInline]
+    
+class SearchKeywordAdmin(admin.ModelAdmin):
+    pass
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageAdminWithKeywords)
