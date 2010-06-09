64771daffe99469aa9d390b1fb2c6873cfe8872b
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-06-09 02:00:22 +0000

    essage

--- DjangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/coltrane/views.py
+++ DjangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/coltrane/views.py
@@ -5,5 +5,5 @@ from simplecms.coltrane.models import Entry
 def entries_index(request):
     return render_to_response('coltrane/entry_index.html',
                               {'entry_list': Entry.objects.all()})
-def entry_detail(request):
+def entry_detail(request, year, month, day, slug):
     return render_to_response('coltrane/entry_index.html')
