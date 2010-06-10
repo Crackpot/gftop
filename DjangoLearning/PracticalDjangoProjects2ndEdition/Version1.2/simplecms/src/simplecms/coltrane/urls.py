631c7383bfea4ef256fcd309f02a6541aec401ad
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-06-10 03:04:33 +0000

    essage

--- DjangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/coltrane/urls.py
+++ DjangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/coltrane/urls.py
@@ -1,9 +1,21 @@
 #coding=utf8
 from django.conf.urls.defaults import *
 from simplecms.coltrane import views
+from django.views.generic.date_based import object_detail
+from simplecms.coltrane.models import Entry
+
+entry_info_dict = {
+    'queryset': Entry.objects.all(),
+    'date_field': 'pub_date',
+}
+
+
 urlpatterns = patterns('',
+    (r'^/$', 'django.views.generic.date_based.archive_index',entry_info_dict),
+
     (r'^/$', views.entries_index),
-    #(P?<slug>[-\w]+)/$
-    (r'^/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>\w+)/$', views.entry_detail),
-    (r'^/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(P?<slug>[-\w]+)/$', views.entries_index),
+    (r'^/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$','django.views.generic.date_based.object_detail', entry_info_dict),
+
+    (r'^/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', object_detail, entry_info_dict),
+    (r'^/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', views.entry_detail),
 )
