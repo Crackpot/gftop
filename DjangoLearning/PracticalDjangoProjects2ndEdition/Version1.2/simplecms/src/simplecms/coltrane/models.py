68a1af2601d764169a4449c76d894193bb48c817
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-06-10 08:25:56 +0000

    essage

--- DjangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/coltrane/models.py
+++ DjangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/coltrane/models.py
@@ -9,9 +9,9 @@ from tagging.fields import TagField
 
 class  Category(models.Model):
     title = models.CharField('标题', max_length = 250,
-                             help_text='Maximum 250 characters.')
+            help_text='Maximum 250 characters.')
     slug = models.SlugField(unique = True,
-                            help_text = "Suggested value automatically generated from title. Must be unique.")
+            help_text = "Suggested value automatically generated from title. Must be unique.")
     description = models.TextField('描述')
 
     class Meta:
@@ -36,43 +36,43 @@ class Entry(models.Model):
         (DRAFT_STATUS, 'Draft'),
         (HIDDEN_STATUS, 'Hidden'),
     )
-    
+
     # 关键字段
     title = models.CharField('标题', max_length = 250,
-                             help_text = "Maximun 250 charcters.")
+            help_text = "Maximun 250 charcters.")
     excerpt = models.TextField('摘录', blank = True,
-                               help_text = "A short summary of the entry. Optional.")
+            help_text = "A short summary of the entry. Optional.")
     body = models.TextField('正文')
     pub_date = models.DateTimeField('发布日期', default = datetime.datetime.now)
-    
+
     # markdown生成html代码
     excerpt_html = models.TextField('摘录html', editable = False, blank = True) # editable为假时不在页面上显示
     body_html = models.TextField('正文html', editable = False, blank = True)
-    
+
     # 元数据
     author = models.ForeignKey(User)
     enable_comments = models.BooleanField('允许评论', default = True)
     featured = models.BooleanField(default = True)
     slug = models.SlugField(unique_for_date = 'pub_date',
-                            help_text = "Suggested value automatically generated form title. Must be unque.")
+            help_text = "Suggested value automatically generated form title. Must be unque.")
     status = models.IntegerField('状态', choices = STATUS_CHOICES, default = LIVE_STATUS,
-                                 help_text = "Only entries with live status will be publicly displayed.")
-    
+            help_text = "Only entries with live status will be publicly displayed.")
+
     # 分类
     categories = models.ManyToManyField(Category)
     tags = TagField('标签', help_text = "Separate tags with spaces.")
-    
+
     class Meta:
         ordering = ['-pub_date', 'title']
         verbose_name = '条目'
         verbose_name_plural = '条目'
-        
+
     def __unicode__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         return "/weblog/%s/%s/" % (self.pub_date.strftime("%Y/%b/%d").lower(), self.slug)
-    
+
     # 重写此model的save方法
     def save(self, force_insert = False, force_update = False):
         self.body_html = markdown(self.body)
