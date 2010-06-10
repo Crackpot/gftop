3704fcd2709f3b55a1e3a1e1a1fcc0866b9aa41d
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-06-10 07:25:38 +0000

    essage

--- DjangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/search/models.py
+++ DjangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/search/models.py
@@ -5,5 +5,6 @@ from django.contrib.flatpages.models import FlatPage
 class SearchKeyword(models.Model):
     keyword = models.CharField('关键词', max_length = 50)
     page = models.ForeignKey(FlatPage)
+    
     def __unicode__(self):
         return self.keyword
