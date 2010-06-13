d77122b842667e93725c33450728329d2b9ff919
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-06-13 13:31:42 +0000

    noting

--- trunk/DjangoLearning/makestory-whhhy/daily/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from views import DailyViews
-
-urlpatterns = patterns('',
-    (r'^(?P<date>\d+-\d+)/$',DailyViews.monthly),
-    (r'^(?P<date>\d+-\d+-\d+)/$',DailyViews.daily),
-    (r'^$',DailyViews.latest),
-)
