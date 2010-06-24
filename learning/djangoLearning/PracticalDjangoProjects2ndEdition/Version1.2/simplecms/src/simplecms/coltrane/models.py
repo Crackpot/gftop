e25032faa1a386464545d4755a587f72fdbca0cb
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-06-24 14:53:12 +0000

    今晚最后修改

--- learning/djangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/coltrane/models.py
+++ learning/djangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/coltrane/models.py
@@ -163,3 +163,33 @@ class Link(models.Model):
                     'day': self.pub_date.strftime('%d'),
                     'slug': self.slug })
     get_absolute_url = models.permalink(get_absolute_url)
+
+
+
+# 后来新添加的代码
+from akismet import Akismet
+from django.contrib.comments.models import Comment
+from django.contrib.comments.signals import comment_will_be_posted
+from django.contrib.sites.models import Site
+from django.utils.encoding import smart_str
+
+# Akismet是最强大的WordPress反垃圾插件
+def moderate_comment(sender, comment, request, **kwargs):
+    if not comment.id:
+        entry = comment.content_object
+        delta = datetime.datetime.now() - entry.pub_date
+        if delta.days > 30:
+            comment.is_public = False
+        else:
+            akismet_api = Akismet(key = settings.AKISMET_API_KEY,
+                                  blog_url = "http:/%s/" % Site.objects.get_current().domain)
+            if akismet_api.verify_key():
+                akismet_data = {'comment_type': 'comment',
+                                'referrer': request.META['HTTP_REFERRER'],
+                                'user_ip': comment.ip_address,
+                                'user-agent': request.META['HTTP_USER_AGENT']}
+                if akismet_api.comment_check(smart_str(comment.comment),
+                                             akismet_data,
+                                             build_data = True):
+                    comment.is_public = False
+comment_will_be_posted.connect(moderate_comment, sender = Comment) # receiver, sender, weak, dispatch_uid
