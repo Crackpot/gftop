eada4f3b909e16a5532a1e4214e1b47ed187cb21
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-06-25 16:06:52 +0000

    今晚最后修改

--- learning/djangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/cab/models.py
+++ learning/djangoLearning/PracticalDjangoProjects2ndEdition/Version1.2/simplecms/src/simplecms/cab/models.py
@@ -1,3 +1,60 @@
+#coding=utf8
+import datetime
 from django.db import models
+from pygments import formatters, highlight, lexers
+from markdown import markdown
+from tagging.fields import TagField
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Language(models.Model):
+    name = models.CharField('名称', max_length = 100)
+    slug = models.SlugField(unique = True)
+    language_code = models.CharField('语言代码', max_length = 50)
+    mime_type = models.CharField('mime类型', max_length = 100)
+    
+    class Meta:
+        ordering = ['name']
+    def __unicode__(self):
+        return self.name
+        
+    def get_absolute_url(self):
+        return ('cab_language_detail', (), {'slug': self.slug})
+    get_absolute_url = models.permalink(get_absolute_url)
+    
+    def get_lexer(self):
+        return lexers.get_lexer_by_name(self.language_code)
+
+class Snippet(models.Model):
+    title = models.CharField('标题', max_length = 255)
+    language = models.ForeignKey(Language)
+    author = models.ForeignKey(User)
+    description = models.TextField()
+    description_html = models.TextField(editable = False)
+    code = models.TextField()
+    highlighted_code = models.TextField(editable = False)
+    tags = TagField()
+    pub_date = models.DateTimeField(editable = False)
+    updated_date = models.DateTimeField(editable = False)
+    
+    class Meta:
+        ordering = ['-pub_date']
+        
+    def __unicode__(self):
+        return self.title
+    
+    def save(self, force_insert = False, force_update = False):
+        if not self.id:
+            self.pub_date = datetime.datetime.now()
+        self.updated_date = datetime.datetime.now()
+        self.description_html = markdown(self.description)
+        self.highlighted_code = self.highlight()
+        super(Snippet, self).save(force_insert, force_update)
+        
+    def get_absolute_url(self):
+        return ('cab_snippet_detail', (), {'object_id': self.id})
+    get_absolute_url = models.permalink(get_absolute_url)
+
+    def highlight(self):
+        return highlight(self.code,
+                         self.language.get_lexer(),
+                         formatters.HtmlFormatter(linenos=True))
