130fb97b5d9974637a3ed6a80548cb3da3d68375
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-07-14 11:08:55 +0000

    创建新的项目sxhsyj，使用框架django1.2

--- learning/pythonLearning/PythonChallenge/src/Level02.py
+++ learning/pythonLearning/PythonChallenge/src/Level02.py
@@ -1,25 +1,28 @@
-#coding=utf-8
+#coding=utf8
 '''
     http://www.pythonchallenge.com/pc/def/ocr.html
 '''
 import string
 class decoder(object):
     def __init__(self):
-        self.myfile=open('Level02.txt')
-        self.content=self.myfile.read()     #对比readlines,readline
-        self.result=''
+        self.myfile = open('Level02.txt')
+        self.content = self.myfile.read()     #对比readlines,readline
+        self.result = ''
+        
     def printContent(self):
-        print '%s\n%s\n%s'%(self.content,type(self.content),len(self.content))
+        print '%s\n%s\n%s' % (self.content, type(self.content), len(self.content))
+        
     def decoder_1(self):
         #这是我的方法
         for i in self.content:
             if i in string.letters:
-                self.result+=i
+                self.result += i
         print self.result
+        
     def decoder_2(self):
         pass
 
-instance=decoder()
+instance = decoder()
 #instance.printContent()
 instance.decoder_1()
 instance.decoder_2()
