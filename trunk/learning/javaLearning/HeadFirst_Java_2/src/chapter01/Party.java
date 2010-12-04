605c1f66fb6308f7a744d576f551b9b1c1ef0b11
gaofeitop <gaofeitop@0700f6e2-4af4-11de-a39b-05eb13f3dc65>
2010-12-04 13:47:33 +0000

    添加HeadFirst

---
+++ trunk/learning/javaLearning/HeadFirst_Java_2/src/chapter01/Party.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package chapter01;
+
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * @author crackpot
+ * 
+ */
+public class Party {
+	public static void main(String[] args) {
+//	}
+
+//	public void buildInvite() {
+		Frame f = new Frame();
+		Label l = new Label("Party at Tim's");
+		Button b = new Button("You bet");
+		Button c = new Button("Shoot me");
+		Panel p = new Panel();
+		p.add(l);
+	}
+}
